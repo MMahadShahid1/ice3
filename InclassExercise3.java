54da786f57bf1fa2b3419744b049f69eee246e6c
MMahadShahid1 <137260302+MMahadShahid1@users.noreply.github.com>
2025-03-09 23:19:20 -0400

    Update InclassExercise3.java

--- InclassExercise3.java
+++ InclassExercise3.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class InclassExercise3{
     public static void main (String[] args){
 
+                
+        System.out.print("Muhammad Mahad ICE 3 ");
+        
         Scanner input = new Scanner(System.in);
         System.out.print("First name: ");
         String fName = input.nextLine();
